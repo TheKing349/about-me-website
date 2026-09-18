787fe8a4ca34163b0202800f9a6387a9b6fa50b8
Aiden Kimmerling <apkimmerling@gmail.com>
2026-09-18 16:42:25 -0500

    Replace users with projects

---
+++ src/prisma/projects.ts
@@ -0,0 +1,13 @@
+import { db } from "./db.ts";
+import { seed } from "./seed.ts";
+
+export { db };
+
+export async function listProjects() {
+  await seed();
+  const projects = await db.orm.public.Project.select("id", "title", "desc", "image", "publishLink", "githubLink", "creationDate", "insights", "tools").all();
+
+  return projects;
+}
+
+export type StarterProjects = Awaited<ReturnType<typeof listProjects>>[number];
