807585045b28cbdccfd6e5c078c1b33d62cb2e93
Aiden Kimmerling <apkimmerling@gmail.com>
2026-09-19 19:20:13 -0500

    Add name and text to nav bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,33 +3,41 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-export default function() {
+export default function Navbar() {
   const pathname = usePathname();
 
   const navLinks = [
     { name: 'Home', href: '/' },
-    { name: 'About Me', href: '/about' },
     { name: 'Projects', href: '/projects' },
     { name: 'Contact', href: '/contact' },
   ];
 
   return (
-    <nav className="flex justify-center gap-4 p-4 pt-6 border-b">
-      {navLinks.map((link) => {
-        const isActive = pathname === link.href;
+    <header className="flex flex-col p-5 items-center border-b">
+      <div className="w-fit mx-auto text-center">
+        <h1 className="text-xl font-bold">Aiden Kimmerling</h1>
+        <p className="text-xs">
+          <span className="block">Sophomore • Computer Science and Math</span>
+          <span className="block">University of Nebraska-Lincoln</span>
+        </p>
+      </div>
+      <nav className="flex gap-4 pt-4">
+        {navLinks.map((link) => {
+          const isActive = pathname === link.href;
 
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={`no-underline hover:underline ${
-              isActive ? 'underline decoration-2 underline-offset-5' : ''
-            }`}
-          >
-            {link.name}
-          </Link>
-        );
-      })}
-    </nav>
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={`no-underline hover:underline ${
+                isActive ? 'underline decoration-2 text-blue-500 underline-offset-5' : ''
+              }`}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </nav>
+    </header>
   );
 }
