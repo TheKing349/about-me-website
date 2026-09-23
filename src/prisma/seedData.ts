0071cba0d8ca2b7e10e86419b2091504bcc06ac3
Aiden Kimmerling <apkimmerling@gmail.com>
2026-09-22 21:56:44 -0500

    Fix id issue

--- src/prisma/seedData.ts
+++ src/prisma/seedData.ts
@@ -39,7 +39,7 @@ export const projects = [
     tools: ["GitHub", "Unity"],
   },
   {
-    id: 2,
+    id: 3,
     title: "Three",
     desc: "Desc Three",
     images: [placeholderImage, placeholderImage, placeholderImage],
